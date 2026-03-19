74c521d57739d904a837e2186171d4e2610a6944
Papercut Truths <mindmingle794@gmail.com>
2026-03-19 16:31:46 +0530

    Update firebase.ts

--- src/contexts/firebase.ts
+++ src/contexts/firebase.ts
@@ -1,8 +1,25 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, collection, query, where, orderBy, onSnapshot, addDoc, serverTimestamp, getDoc, doc } from 'firebase/firestore';
+import { signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
+export type { User as FirebaseUser } from 'firebase/auth';
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const auth = getAuth(app);
+
+export { 
+  collection, 
+  query, 
+  where, 
+  orderBy, 
+  onSnapshot, 
+  addDoc, 
+  serverTimestamp, 
+  getDoc, 
+  doc,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged
+};
